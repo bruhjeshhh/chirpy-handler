130b846ada01ebf2bbce123569952561dfe8da1c
Brajesh Mohanty <mohantybrajesh4@gmail.com>
2026-01-21 01:05:29 +0530

    Stateful handlers

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 //	type Server struct {
 //		Addr string
 //		Handler Handler
 //	}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
 	ptr := http.NewServeMux()
 
@@ -16,9 +23,14 @@ func main() {
 		Addr:    ":8080",
 		Handler: ptr,
 	}
+	var cfg apiConfig
 	wrappedHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
-	ptr.Handle("/app/", wrappedHandler)
+	ptr.Handle("/app/", cfg.middlewareMetricsInc(wrappedHandler))
+	// ptr.Handle("/metrics", cfg.middlewareMetricsInc(http.HandlerFunc(cfg.fetchmetric)))
+
 	ptr.HandleFunc("/healthz", app)
+	ptr.HandleFunc("/reset", cfg.resetmetric)
+	ptr.HandleFunc("/metrics", cfg.fetchmetric)
 
 	log.Printf("we ballin")
 	log.Fatal(srv.ListenAndServe())
@@ -29,4 +41,25 @@ func app(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) resetmetric(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	cfg.fileserverHits.Add(-hits)
+}
+
+func (cfg *apiConfig) fetchmetric(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	hits := cfg.fileserverHits.Load()
+	resp := fmt.Sprintf("Hits: %d", hits)
+	w.Write([]byte(resp))
+
 }
