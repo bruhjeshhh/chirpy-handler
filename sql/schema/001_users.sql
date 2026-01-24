cb8ddcfec906d257208b9b62331359b0c9d7815c
Brajesh Mohanty <mohantybrajesh4@gmail.com>
2026-01-25 01:42:07 +0530

    sqlc init

---
+++ sql/schema/001_users.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+CREATE TABLE users(
+	id uuid primary key,
+	created_at timestamp not null,
+	updated_at timestamp not null,
+	email text unique not null
+);
+
+-- +goose Down
+DROP TABLE users;
+
+
+ 
