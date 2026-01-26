79b12b9381c3119f7f303ec62d7ead16a2bf1796
Brajesh Mohanty <mohantybrajesh4@gmail.com>
2026-01-26 16:45:05 +0530

    jwt token

---
+++ internal/auth/jwt_Handler.go
@@ -0,0 +1,25 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func GetBearerToken(headers http.Header) (string, error) {
+	token := headers.Get("Authorization")
+
+	ans := strings.HasPrefix(token, "Bearer ")
+
+	if ans == false {
+		return "", fmt.Errorf("auth header missing")
+	}
+
+	trimmed := strings.TrimSpace(strings.TrimPrefix(token, "Bearer "))
+	if trimmed == "" {
+		return "", fmt.Errorf("the auth token is invalid")
+	}
+
+	return trimmed, nil
+
+}
